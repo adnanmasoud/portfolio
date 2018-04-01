7cf96578ee5fa61965b4782f53ea01bf2a3702b7
yazan <yazan@datumcorp.com>
2018-04-01 18:36:13 +0800

    fixed error in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AdnanModule } from './adnan/adnan.module';
-import { YazanComponent } from './yazan/yazan.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    YazanComponent
   ],
   imports: [
     BrowserModule, AdnanModule
